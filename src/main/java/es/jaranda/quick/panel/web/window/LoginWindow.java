a105a53ee758a8f59e127a49e0c845445a935b0c
jorge-aranda <jorgearanda123@gmail.com>
2015-10-16 01:35:48 +0200

    - Include an example of a M[VP] Milestone: Generate common patrol for all application

--- src/main/java/es/jaranda/quick/panel/web/window/LoginWindow.java
+++ src/main/java/es/jaranda/quick/panel/web/window/LoginWindow.java
@@ -7,6 +7,7 @@ import com.vaadin.ui.Window;
 import es.jaranda.quick.panel.web.util.I18nHelper;
 import static es.jaranda.quick.panel.constants.i18n.DemoI18nConstants.*;
 import static es.jaranda.quick.panel.constants.i18n.LoginI18nConstants.*;
+import es.jaranda.quick.panel.web.presenter.LoginPresenter;
 import es.jaranda.quick.panel.web.utils.NotificationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import static 
@@ -22,11 +23,17 @@ public class LoginWindow extends Window {
     
     private final I18nHelper i18n;
     
+    /* Presenter Layer */
+    private final LoginPresenter presenter;
+    
     @Autowired
-    public LoginWindow(I18nHelper i18n) {
+    public LoginWindow(I18nHelper i18n, LoginPresenter presenter) {
         super(i18n.getMessage(CAPTION));
         
         this.i18n = i18n;
+        // Inicialize presenter
+        this.presenter = presenter;
+        this.presenter.setView(this);
         
         setUp();
 
@@ -44,9 +51,9 @@ public class LoginWindow extends Window {
         this.setWidth(WIDTH_SIZE);
         this.setHeight(HEIGHT_SIZE);
     }
-    
-    private String getMessage(String code) {
-        return i18n.getMessage(code);
+        
+    private String getMessage(String code, String... args) {
+        return i18n.getMessage(code, args);
     }
     
     // XXX remove demo
@@ -54,7 +61,10 @@ public class LoginWindow extends Window {
     
     private void prepareDemo() {
         this.setContent(new Button(getMessage(CLICK_ME), 
-            e -> NotificationUtils.showInfoMessage(getMessage(HELLO)))
-        );
+                presenter::buttonClickMe));
+    }
+    
+    public void displayDemoMessage() {
+        NotificationUtils.showInfoMessage(i18n.getMessage(HELLO));
     }
 }
